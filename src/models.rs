f55b056ddfef6217c3edc8a85021e6e3b1d45ff3
Karl <karl.wolffgang@racemap.com>
2023-07-15 14:02:59 +0200

    fix custom type

--- src/models.rs
+++ src/models.rs
@@ -1,48 +1,46 @@
+use crate::schema::sql_types::CellsRadioEnum;
 use chrono::NaiveDateTime;
 use diesel::deserialize::FromSql;
-use diesel::helper_types::IsNull;
 use diesel::mysql::{Mysql, MysqlValue};
 use diesel::prelude::*;
-use diesel::serialize::{Output, ToSql};
+use diesel::serialize::{IsNull, Output, ToSql};
 use diesel::*;
-use diesel_derive_enum::DbEnum;
+
 use serde_with::formats::Flexible;
 use serde_with::BoolFromInt;
 use serde_with::TimestampSeconds;
+use std::io::Write;
 
-// #[derive(SqlType)]
-// #[diesel(mysql_type(name = "RadioType"))]
-// pub struct RadioType;
-
-#[derive(Debug, serde::Deserialize, DbEnum)]
+#[derive(Debug, serde::Deserialize, FromSqlRow, AsExpression)]
+#[sql_type = "CellsRadioEnum"]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
-enum Radio {
+pub enum Radio {
     Gsm,
     Umts,
     Lte,
 }
 
-// impl ToSql<Radio, Mysql> for Radio {
-//     fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Mysql>) -> serialize::Result {
-//         match *self {
-//             Radio::Umts => out.write_all(b"UMTS")?,
-//             Radio::Gsm => out.write_all(b"GSM")?,
-//             Radio::Lte => out.write_all(b"LTE")?,
-//         }
-//         Ok(IsNull::No)
-//     }
-// }
+impl ToSql<CellsRadioEnum, Mysql> for Radio {
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Mysql>) -> serialize::Result {
+        match *self {
+            Radio::Umts => out.write_all(b"UMTS")?,
+            Radio::Gsm => out.write_all(b"GSM")?,
+            Radio::Lte => out.write_all(b"LTE")?,
+        }
+        Ok(IsNull::No)
+    }
+}
 
-// impl FromSql<Radio, Mysql> for Radio {
-//     fn from_sql(bytes: MysqlValue<'_>) -> deserialize::Result<Self> {
-//         match bytes.as_bytes() {
-//             b"UMTS" => Ok(Radio::Umts),
-//             b"GSM" => Ok(Radio::Gsm),
-//             b"LTE" => Ok(Radio::Lte),
-//             _ => Err("Unrecognized enum variant".into()),
-//         }
-//     }
-// }
+impl FromSql<CellsRadioEnum, Mysql> for Radio {
+    fn from_sql(bytes: MysqlValue<'_>) -> deserialize::Result<Self> {
+        match bytes.as_bytes() {
+            b"UMTS" => Ok(Radio::Umts),
+            b"GSM" => Ok(Radio::Gsm),
+            b"LTE" => Ok(Radio::Lte),
+            _ => Err("Unrecognized enum variant".into()),
+        }
+    }
+}
 
 #[serde_with::serde_as]
 #[derive(Queryable, Selectable)]
